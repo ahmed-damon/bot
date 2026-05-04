a7bb07dcf858490a29e4c542d9457c1de044da28
ahmed-damon <ax2759009@gmail.com>
2026-05-04 14:42:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// تشغيل الملفات الثابتة (الصور والصفحات)
-app.use(express.static(path.join(__index, '/')));
+// تشغيل الملفات الثابتة من المجلد الرئيسي
+app.use(express.static(path.join(__dirname, '/')));
 
-// الصفحة الرئيسية (الواجهة)
+// الصفحة الرئيسية (الواجهة التي تحتوي على صورتك وزر المطور)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// صفحة البوربوينت (الأمير الصغير)
+// صفحة عرض الأمير الصغير والأسئلة
 app.get('/slides', (req, res) => {
   res.sendFile(path.join(__dirname, 'slides.html'));
 });
